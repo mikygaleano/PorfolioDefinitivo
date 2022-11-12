f04d242616221791db240e80f45a84e424c9cb23
mikygaleano <creatormikeonline@gmail.com>
2022-11-12 01:02:22 -0300

    porfolio

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 
 
+function hamburgue () {
+    const $btnMenu = document.querySelector('.button-menu'),
+    $conteinerMenu = document.querySelector('.nav_conteinerMenu');
 
+    $btnMenu.addEventListener("click", () => {
+        $conteinerMenu.classList.toggle("visible");
+    });
+}
+
+
+hamburgue();
