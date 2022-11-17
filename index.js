7a4db72e90c4f03e8e8452e9891ad7b6ab3dc1ad
mikygaleano <creatormikeonline@gmail.com>
2022-11-16 21:39:08 -0300

    porfolio

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+/* variables */
+const $navMenu = document.querySelector('.nav-menu'),
+$btnHamburguer = document.querySelector('.btn-hamburguer'),
+$linkMenu = document.getElementsByClassName('link-item');
+
+/* funciones */
+function hamburguer () {
+    $btnHamburguer.addEventListener('click', () => {
+        $navMenu.classList.toggle('colapse')
+    });
+
+    for (let i = 0; i < $linkMenu.length; i++) {
+        $linkMenu[i].addEventListener('click', () => {
+            $navMenu.classList.toggle('colapse')
+        });
+    };
+};
+
+/* llamados */
+hamburguer();
