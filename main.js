f018910c7b2b70b8f09ad51d8168f255cb21f082
mikygaleano <creatormikeonline@gmail.com>
2023-01-04 00:29:19 -0300

    menu-hamburg

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const d = document,
+btnHamburg = d.querySelector('.btn-hamburg'),
+panelMenuMobile = d.querySelector('.menu-mobile');
+
+
+function menuHamburg () {
+    btnHamburg.addEventListener('click', () => {
+        if (panelMenuMobile.classList.contains('add-colapse')) {
+            panelMenuMobile.classList.remove('add-colapse');
+        } else {
+            panelMenuMobile.classList.add('add-colapse');
+        }
+    });
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    menuHamburg();
+})
